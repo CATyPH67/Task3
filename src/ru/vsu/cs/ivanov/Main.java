2f017c16d555ba36c84fce81b463c46bdf2d8879
kirill <031kirill2a@mail.ru>
2020-09-22 21:16:26 +0300

    reworked for classes

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -4,56 +4,109 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
+    public static void main (String[] args) {
+        Program.printColorForPoint(0, 0);
+        Program.printColorForPoint(-4, 1);
+        Program.printColorForPoint(1, 2);
+        Program.printColorForPoint(3, 3);
+        Program.printColorForPoint(1, 7);
+        Program.printColorForPoint(-1, 4);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("enter x for point: ");
         double x = scanner.nextDouble();
         System.out.print("enter y for point: ");
         double y = scanner.nextDouble();
-        System.out.println(findArea(x, y));
+        Program.printColorForPoint(x, y);
     }
 
-    enum Color {
+}
+
+class Program {
+
+    enum SimpleColor {
         BLUE, GREEN, WHITE, ORANGE, GRAY, YELLOW
     }
 
-    static Color findArea (double x, double y) {
-        if ((isParabola(x, y) && isSquare(x, y) && isCircleBig(x, y))
-                                    || (isParabola(x, y) && !isSquare(x, y) && !isCircleBig(x, y))) {
-            return Color.GREEN;
+    private static class Square {
+        double x0; // x for left down point
+        double y0; // y for left down point
+        double a; // square side length
+
+        private Square(double x0, double y0, double a) {
+            this.x0 = x0;
+            this.y0 = y0;
+            this.a = a;
         }
-        if (isParabola(x, y) && !isCircleBig(x, y) && !isCircleSmall(x, y)) {
-            return Color.BLUE;
+
+        private boolean isSquare(double x, double y) {
+            return (x >= x0) && (x <= (x0 + a)) && (y >= y0) && (y <= (y0 + a));
         }
-        if ((isCircleBig(x, y) && isSquare(x, y) && !isParabola(x, y))
-                                    || (isCircleBig(x, y) && !isParabola(x, y) && (y < 5))) {
-            return Color.WHITE;
-        }
-        if ((isSquare(x, y) && !isParabola(x, y) && !isCircleBig(x, y) && !isCircleSmall(x, y))
-                                    || (isCircleBig(x, y) && isParabola(x, y) && !isSquare(x, y))) {
-            return Color.GRAY;
-        }
-        if ((isCircleSmall(x, y) && !isParabola(x, y))
-                                    || (isCircleBig(x, y) && !isParabola(x, y) && !isSquare(x, y) && (y > 5))) {
-            return Color.YELLOW;
-        }
-        return Color.ORANGE;
     }
 
-    static boolean isSquare (double x, double y) {
-        return (x >= -2) && (x <= 6) && (y >= 1) && (y <= 9);
+    private static class Parabola {
+        double x0;
+        double y0;
+        double a;
+
+        private Parabola(double x0, double y0, double a) {
+            this.x0 = x0;
+            this.y0 = y0;
+            this.a = a;
+        }
+
+        private boolean isParabola(double x, double y) {
+            return (a * Math.pow(y - y0, 2) + x0) >= x;
+        }
     }
 
-    static boolean isParabola (double x, double y) {
-        return (x <= 4) && ((-1 * (y - 6) * (y - 6) + 4) >= x);
+    private static class Circle {
+        double x0;
+        double y0;
+        double r;
+
+        private Circle (double x0, double y0, double r) {
+            this.x0 = x0;
+            this.y0 = y0;
+            this.r = r;
+        }
+
+        private boolean isCircle(double x, double y) {
+            return (Math.pow(x - x0, 2) + Math.pow(y - y0, 2)) <= Math.pow (r, 2);
+        }
     }
 
-    static boolean isCircleBig (double x, double y) {
-        return ((x + 4) * (x + 4) + (y - 5) * (y - 5)) <= 25;
+    private static final Square square = new Square (-2, 1, 8);
+    private static final Parabola parabola = new Parabola (4,6,-1);
+    private static final Circle circleBig = new Circle (-4,5,5);
+    private static final Circle circleSmall = new Circle (3,4,2);
+
+    public static SimpleColor getColor(double x, double y) {
+
+        if ((parabola.isParabola(x, y) && square.isSquare(x, y) && circleBig.isCircle(x, y))
+                || (parabola.isParabola(x, y) && !square.isSquare(x, y) && !circleBig.isCircle(x, y))) {
+            return SimpleColor.GREEN;
+        }
+        if (parabola.isParabola(x, y) && !circleBig.isCircle(x, y) && !circleSmall.isCircle(x, y)) {
+            return SimpleColor.BLUE;
+        }
+        if ((circleBig.isCircle(x, y) && square.isSquare(x, y) && !parabola.isParabola(x, y))
+                || (circleBig.isCircle(x, y) && !parabola.isParabola(x, y) && (y < 5))) {
+            return SimpleColor.WHITE;
+        }
+        if ((square.isSquare(x, y) && !parabola.isParabola(x, y) && !circleBig.isCircle(x, y) && !circleSmall.isCircle(x, y))
+                || (circleBig.isCircle(x, y) && parabola.isParabola(x, y) && !square.isSquare(x, y))) {
+            return SimpleColor.GRAY;
+        }
+        if ((circleSmall.isCircle(x, y) && !parabola.isParabola(x, y))
+                || (circleBig.isCircle(x, y) && !parabola.isParabola(x, y) && !square.isSquare(x, y) && (y > 5))) {
+            return SimpleColor.YELLOW;
+        }
+        return SimpleColor.ORANGE;
     }
 
-    static boolean isCircleSmall (double x, double y) {
-        return ((x - 3) * (x - 3) + (y - 4) * (y - 4)) <= 4;
+    public static void printColorForPoint(double x, double y) {
+        SimpleColor color = Program.getColor(x, y);
+        System.out.printf("(%.1f, %.1f) -> %S%n", x, y, color);
     }
 
 }
