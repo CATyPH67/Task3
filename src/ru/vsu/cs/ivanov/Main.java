4e646e9b013bab166d1362fbd43ffcfca0f5419e
kirill <031kirill2a@mail.ru>
2020-09-30 11:10:07 +0300

    reworked for classes

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -2,10 +2,8 @@ package ru.vsu.cs.ivanov;
 
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main (String[] args)
-    {
+public class Main {
+    public static void main (String[] args) {
         Program.printColorForPoint(0, 0);
         Program.printColorForPoint(-4, 1);
         Program.printColorForPoint(1, 2);
--- src/ru/vsu/cs/ivanov/Program.java
+++ src/ru/vsu/cs/ivanov/Program.java
@@ -3,23 +3,20 @@ package ru.vsu.cs.ivanov;
 import java.util.Locale;
 import java.util.Scanner;
 
-class Program
-{
+class Program {
     public static double enterPointCoordinate(char coordinateName) {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Input %S: ", coordinateName);
         return scanner.nextDouble();
     }
 
-    public static void printColorForPoint(double x, double y)
-    {
+    public static void printColorForPoint(double x, double y) {
         Locale.setDefault(Locale.ROOT);
         SimpleColor color = Program.getColor(x, y);
         System.out.printf("(%.1f, %.1f) -> %S%n", x, y, color);
     }
 
-    enum SimpleColor
-    {
+    enum SimpleColor {
         BLUE, GREEN, WHITE, ORANGE, GRAY, YELLOW
     }
 
@@ -28,33 +25,32 @@ class Program
     private static Circle circleBig = new Circle (-4,5,5);
     private static Circle circleSmall = new Circle (3,4,2);
 
-    private static SimpleColor getColor(double x, double y)
-    {
+    private static SimpleColor getColor(double x, double y) {
         if ((parabola.isParabola(x, y) && square.isSquare(x, y) && circleBig.isCircle(x, y))
-                || (parabola.isParabola(x, y) && !square.isSquare(x, y) && !circleBig.isCircle(x, y)))
-        {
+                || (parabola.isParabola(x, y) && !square.isSquare(x, y) && !circleBig.isCircle(x, y))) {
             return SimpleColor.GREEN;
         }
-        if (parabola.isParabola(x, y) && !circleBig.isCircle(x, y) && !circleSmall.isCircle(x, y))
-        {
+
+        if (parabola.isParabola(x, y) && !circleBig.isCircle(x, y) && !circleSmall.isCircle(x, y)) {
             return SimpleColor.BLUE;
         }
+
         if ((circleBig.isCircle(x, y) && square.isSquare(x, y) && !parabola.isParabola(x, y))
-                || (circleBig.isCircle(x, y) && !parabola.isParabola(x, y) && (circleBig.isLowerHalfCircle(y))))
-        {
+                || (circleBig.isCircle(x, y) && !parabola.isParabola(x, y) && (circleBig.isLowerHalfCircle(y)))) {
             return SimpleColor.WHITE;
         }
+
         if ((square.isSquare(x, y) && !parabola.isParabola(x, y) && !circleBig.isCircle(x, y)
                 && !circleSmall.isCircle(x, y)) || (circleBig.isCircle(x, y)
-                && parabola.isParabola(x, y) && !square.isSquare(x, y)))
-        {
+                && parabola.isParabola(x, y) && !square.isSquare(x, y))) {
             return SimpleColor.GRAY;
         }
+
         if ((circleSmall.isCircle(x, y) && !parabola.isParabola(x, y)) || (circleBig.isCircle(x, y)
-                && !parabola.isParabola(x, y) && !square.isSquare(x, y) && (circleBig.isUpperHalfCircle(y))))
-        {
+                && !parabola.isParabola(x, y) && !square.isSquare(x, y) && (circleBig.isUpperHalfCircle(y)))) {
             return SimpleColor.YELLOW;
         }
+
         return SimpleColor.ORANGE;
     }
 }
