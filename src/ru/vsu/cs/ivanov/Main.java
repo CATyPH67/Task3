1c807fb218e7c3ff83714bdb6ca2a34abadb6d66
kirill <031kirill2a@mail.ru>
2020-10-07 19:35:33 +0300

    reworked for classes

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -8,25 +8,15 @@ public class Main {
         Locale.setDefault(Locale.ROOT);
         Picture picture = new Picture(new Square(-2, 1, 8), new HorizontalParabola (4,6,-1),
                                         new Circle (-4,5,5), new Circle (3,4,2));
-        test(new double[6][2], picture);
+        test(picture);
         double x = enterPointCoordinate('x');
         double y = enterPointCoordinate('y');
         outputColorForPoint(x, y, picture);
     }
 
-    private static void test (double[][] arrayOfPoints, Picture picture) {
-        arrayOfPoints[0][0] = 0;
-        arrayOfPoints[0][1] = 0;
-        arrayOfPoints[1][0] = -4;
-        arrayOfPoints[1][1] = 1;
-        arrayOfPoints[2][0] = 1;
-        arrayOfPoints[2][1] = 2;
-        arrayOfPoints[3][0] = 3;
-        arrayOfPoints[3][1] = 3;
-        arrayOfPoints[4][0] = 1;
-        arrayOfPoints[4][1] = 7;
-        arrayOfPoints[5][0] = -1;
-        arrayOfPoints[5][1] = 4;
+    private static void test (Picture picture) {
+        double[][] arrayOfPoints = {{0,-4, 1, 3, 1,-1},
+                                    {0, 1, 2, 3, 7, 4}};
         for (int i = 0; i <= 5; i++) {
             outputColorForPoint(arrayOfPoints[i][0], arrayOfPoints[i][1], picture);
         }
